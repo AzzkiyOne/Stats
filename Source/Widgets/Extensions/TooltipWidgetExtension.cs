bd2349ccd232b97d133feaa6e70e024820c95199
Dmitry "Azzkiy <gwmdmitry@gmail.com>
2025-05-18 15:18:01 +0300

    Minor optimization

--- Source/Widgets/Extensions/TooltipWidgetExtension.cs
+++ Source/Widgets/Extensions/TooltipWidgetExtension.cs
@@ -5,20 +5,14 @@ namespace Stats.Widgets.Extensions;
 
 public sealed class TooltipWidgetExtension : WidgetExtension
 {
-    private readonly string Text;
-    internal TooltipWidgetExtension(Widget widget, string text) : base(widget)
+    private readonly TipSignal Tip;
+    internal TooltipWidgetExtension(Widget widget, string tip) : base(widget)
     {
-        Text = text;
+        Tip = tip;
     }
     public override void Draw(Rect rect, Vector2 containerSize)
     {
-        // Although this is a duplicate check, things should be faster on
-        // average, because TooltipHandler.TipRegion takes TipSignal as an
-        // argument, which has an implicit conversion from string.
-        if (Mouse.IsOver(rect))
-        {
-            TooltipHandler.TipRegion(rect, Text);
-        }
+        TooltipHandler.TipRegion(rect, Tip);
 
         Widget.Draw(rect, containerSize);
     }
