500b78d936054c3237e246924e0246a8b6582abb
Dmitry "Azzkiy <gwmdmitry@gmail.com>
2025-05-24 13:18:22 +0300

    Recreational buildings table

---
+++ Source/ThingTable/ColumnWorkers/BuildingRecreationTypeColumnWorker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Stats.Widgets;
+
+namespace Stats.ThingTable;
+
+public sealed class BuildingRecreationTypeColumnWorker : ColumnWorker<ThingAlike>
+{
+    public BuildingRecreationTypeColumnWorker(ColumnDef columnDef) : base(columnDef, ColumnCellStyle.String)
+    {
+    }
+    private static readonly Func<ThingAlike, JoyKindDef?> GetJoyKindDef =
+    FunctionExtensions.Memoized((ThingAlike thing) =>
+    {
+        return thing.Def.building?.joyKind;
+    });
+    public override Widget? GetTableCellWidget(ThingAlike thing)
+    {
+        var joyKindDef = GetJoyKindDef(thing);
+
+        if (joyKindDef == null)
+        {
+            return null;
+        }
+
+        return new Label(joyKindDef.LabelCap);
+    }
+    public override FilterWidget<ThingAlike> GetFilterWidget(IEnumerable<ThingAlike> tableRecords)
+    {
+        var joyKindDefs = tableRecords
+            .Select(GetJoyKindDef)
+            .Distinct()
+            .OrderBy(joyKindDef => joyKindDef?.label);
+
+        return new OneToManyFilter<ThingAlike, JoyKindDef?>(
+            GetJoyKindDef,
+            joyKindDefs,
+            joyKindDef => new Label(joyKindDef == null ? "" : joyKindDef.LabelCap)
+        );
+    }
+    public override int Compare(ThingAlike thing1, ThingAlike thing2)
+    {
+        var joyKindDefLabel1 = GetJoyKindDef(thing1)?.label;
+        var joyKindDefLabel2 = GetJoyKindDef(thing2)?.label;
+
+        return Comparer.Default.Compare(joyKindDefLabel1, joyKindDefLabel2);
+    }
+}
---
+++ Source/ThingTable/TableWorkers/RecreationalBuildingsTableWorker.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Stats.ThingTable;
+
+public sealed class RecreationalBuildingsTableWorker : TableWorker
+{
+    public RecreationalBuildingsTableWorker(TableDef tableDef) : base(tableDef)
+    {
+    }
+    protected override bool IsValidThingDef(ThingDef thingDef)
+    {
+        return thingDef.statBases?.GetStatValueFromList(StatDefOf.JoyGainFactor, 0f) > 0f;
+    }
+}
