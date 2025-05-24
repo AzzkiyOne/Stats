c577dc292afeade83b64e6ad4d4f89a46d937331
Dmitry "Azzkiy <gwmdmitry@gmail.com>
2025-05-24 13:20:09 +0300

    Beds table

---
+++ Source/ThingTable/ColumnWorkers/BedFitsSmallAnimalsColumnWorker.cs
@@ -0,0 +1,12 @@
+﻿namespace Stats.ThingTable;
+
+public sealed class BedFitsSmallAnimalsColumnWorker : BooleanColumnWorker<ThingAlike>
+{
+    public BedFitsSmallAnimalsColumnWorker(ColumnDef columndef) : base(columndef, false)
+    {
+    }
+    protected override bool GetValue(ThingAlike thing)
+    {
+        return thing.Def.building?.bed_humanlike == false;
+    }
+}
---
+++ Source/ThingTable/TableWorkers/BedsTableWorker.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace Stats.ThingTable;
+
+public sealed class BedsTableWorker : TableWorker
+{
+    public BedsTableWorker(TableDef tableDef) : base(tableDef)
+    {
+    }
+    protected override bool IsValidThingDef(ThingDef thingDef)
+    {
+        return thingDef.IsBed;
+    }
+}
