8f3f5ee48f29a35bd1a47defe3e29f9881c7bd07
Dmitry "Azzkiy <gwmdmitry@gmail.com>
2025-05-24 13:20:40 +0300

    Chairs table

---
+++ Source/ThingTable/TableWorkers/ChairsTableWorker.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace Stats.ThingTable;
+
+public sealed class ChairsTableWorker : TableWorker
+{
+    public ChairsTableWorker(TableDef tableDef) : base(tableDef)
+    {
+    }
+    protected override bool IsValidThingDef(ThingDef thingDef)
+    {
+        return thingDef.building?.isSittable == true;
+    }
+}
