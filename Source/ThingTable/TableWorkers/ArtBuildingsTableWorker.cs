2a99bae0744a52044df0701a16be8b29e08d3a37
Dmitry "Azzkiy <gwmdmitry@gmail.com>
2025-05-24 13:21:18 +0300

    Art buildings table

---
+++ Source/ThingTable/TableWorkers/ArtBuildingsTableWorker.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace Stats.ThingTable;
+
+public sealed class ArtBuildingsTableWorker : TableWorker
+{
+    public ArtBuildingsTableWorker(TableDef tableDef) : base(tableDef)
+    {
+    }
+    protected override bool IsValidThingDef(ThingDef thingDef)
+    {
+        return thingDef.IsArt;
+    }
+}
