60869c71c053e8047cef293b5439ae746fed4240
Dmitry "Azzkiy <gwmdmitry@gmail.com>
2025-05-26 00:23:40 +0300

    Widgets: Borders affect widget size

--- Source/Widgets/Extensions/BorderBottomWidgetExtension.cs
+++ Source/Widgets/Extensions/BorderBottomWidgetExtension.cs
@@ -11,6 +11,20 @@ public sealed class BorderBottomWidgetExtension : WidgetExtension
         Thickness = thickness;
         Color = color;
     }
+    protected override Vector2 CalcSize(Vector2 containerSize)
+    {
+        var size = base.CalcSize(containerSize);
+        size.y += Thickness;
+
+        return size;
+    }
+    protected override Vector2 CalcSize()
+    {
+        var size = base.CalcSize();
+        size.y += Thickness;
+
+        return size;
+    }
     public override void Draw(Rect rect, Vector2 containerSize)
     {
         if (Event.current.type == EventType.Repaint)
@@ -20,6 +34,8 @@ public sealed class BorderBottomWidgetExtension : WidgetExtension
                 Color.AdjustedForGUIOpacity()
             );
         }
+
+        rect.height -= Thickness;
 
         Widget.Draw(rect, containerSize);
     }
--- Source/Widgets/Extensions/BorderWidgetExtension.cs
+++ Source/Widgets/Extensions/BorderWidgetExtension.cs
@@ -11,6 +11,22 @@ public sealed class BorderWidgetExtension : WidgetExtension
         Thickness = thickness;
         Color = color;
     }
+    protected override Vector2 CalcSize(Vector2 containerSize)
+    {
+        var size = base.CalcSize(containerSize);
+        size.x += Thickness * 2;
+        size.y += Thickness * 2;
+
+        return size;
+    }
+    protected override Vector2 CalcSize()
+    {
+        var size = base.CalcSize();
+        size.x += Thickness * 2;
+        size.y += Thickness * 2;
+
+        return size;
+    }
     public override void Draw(Rect rect, Vector2 containerSize)
     {
         if (Event.current.type == EventType.Repaint)
@@ -31,6 +47,11 @@ public sealed class BorderWidgetExtension : WidgetExtension
             verRect.x = rect.xMax - Thickness;
             Verse.Widgets.DrawBoxSolid(verRect, color);
         }
+
+        rect.x += Thickness;
+        rect.y += Thickness;
+        rect.width -= Thickness * 2;
+        rect.height -= Thickness * 2;
 
         Widget.Draw(rect, containerSize);
     }
