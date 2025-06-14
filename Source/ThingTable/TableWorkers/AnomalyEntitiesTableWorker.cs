4c1b7d03bf2e80e32a0ac1f0dc1655e6a792564a
Dmitry "Azzkiy <gwmdmitry@gmail.com>
2025-06-14 15:57:07 +0300

    Anomaly entities table

---
+++ Source/ThingTable/TableWorkers/AnomalyEntitiesTableWorker.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace Stats.ThingTable;
+
+public sealed class AnomalyEntitiesTableWorker : TableWorker
+{
+    public AnomalyEntitiesTableWorker(TableDef tableDef) : base(tableDef)
+    {
+    }
+    protected override bool IsValidThingDef(ThingDef thingDef)
+    {
+        return thingDef is { race.IsAnomalyEntity: true, IsCorpse: false };
+    }
+}
